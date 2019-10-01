b6a2f03a03d75aff2452e99983ae746af39560c8
Jeff Lawson <jeff@bovine.net>
2019-10-01 02:19:34 -0500

    update version history in comments

--- plat/win/msvcver.cmd
+++ plat/win/msvcver.cmd
@@ -13,7 +13,8 @@ rem   15 = cl 15.00.xxxx = Visual Studio 2008 (VC9)
 rem   16 = cl 16.00.xxxx = Visual Studio 2010 (VC10)
 rem   17 = cl 17.00.xxxx = Visual Studio 2012 (VC11)
 rem   18 = cl 18.00.xxxx = Visual Studio 2013 (VC12)
-rem   19 = cl 18.00.xxxx = Visual Studio 2015 (VC14)
+rem   19 = cl 19.00.xxxx = Visual Studio 2015 (VC14)
+rem   19 = cl 19.23.xxxx = Visual Studio 2019
 
 for %%i in (cl.exe) do if "%%~$PATH:i"=="" goto notfound
 
